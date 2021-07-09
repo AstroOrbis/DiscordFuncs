f2a9c2eaa13951a009a2fafab66bae1d07fed591
Astro Orbis <astroorbis@gmail.com>
2021-07-09 02:13:20 +0000

    Decided to change the name to somethin' a lil' snazzier ^w^

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # About
 ---
 
-DiscordDefs is a small project made to facilitate development of Discord.py projects.
+DiscordFuncs is a small project made to facilitate development of Discord.py projects.
 
 This project requires Discord.py to be installed, and will install it if it is not present on the system.
 
 # Usage
 ---
-- Use the command `pip3 install DiscordDefs` to install the package.
-- Make sure the line `import DiscordDefs` is somewhere at the top of your file.
+- Use the command `pip3 install DiscordFuncs` to install the package.
 - Call functions like any other function in Python! 
 
 # Dev Note
@@ -16,13 +15,13 @@ Thank you for using this package! Please credit it in your bot's `help` command 
 
 # Modules
 
-### botInfo() (`from DiscordDefs import Bot`)
+### botInfo() (`from DiscordFuncs import Bot`)
 - Prints useful information about your system and the bot to the console.
 
-### setIntent(intentName, Value) (`from DiscordDefs import Bot`)
+### setIntent(intentName, Value) (`from DiscordFuncs import Bot`)
 - `Value` must be `True` or `False`. Anything else will return an error.
 - `intentName` has to be one of the intents listed [here](https://discord.com/developers/docs/topics/gateway#gateway-intents).
 
-### sendMessage(url, content) (`from DiscordDefs import Webhook`)
+### sendMessage(url, content) (`from DiscordFuncs import Webhook`)
 - `url` has to be a valid Discord webhook URL
 - `content` has to be a valid string of text, or a variable that corresponds to one.
--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fhandle:
     long_description = fhandle.read() # Your README.md file will be used as the long description!
 
-setuptools.setup(
-    name="DiscordDefs", # Put your username here!
+setup(
+    name="DiscordFuncs", # Put your username here!
     version="0.0.1", # The version of your package!
     author="Astro Orbis", # Your name here!
     author_email="astroorbis@gmail.com", # Your e-mail here!
     description="Some small discord.py functions for bot developers!", # A short description here!
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AstroOrbis/DiscordDefs", # Link your package website here! (most commonly a GitHub repo)
-    packages=setuptools.find_packages(), # A list of all packages for Python to distribute!
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ], # Enter meta data into the classifiers list!
+    url="https://github.com/AstroOrbis/DiscordFuncs", # Link your package website here! (most commonly a GitHub repo)
+    packages=find_packages(), # A list of all packages for Python to distribute!
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent",], # Enter meta data into the classifiers list!
     python_requires='>=3.6', # The version requirement for Python to run your package!
     keywords=['python3', 'discord', 'discord bot', 'discord webhook'],
     install_requires=[
