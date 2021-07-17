3abffb14f181deb950919402376badf07805511d
AstroOrbis <astroorbis@gmail.com>
2021-07-17 17:26:33 +0000

    Removed some files and updated the Setup files.

--- Setup/setup.bat
+++ Setup/setup.bat
@@ -1,9 +1,13 @@
 @ECHO OFF 
+
 ECHO Installing packages...
+
 pip3 install discord
 pip3 install requests
 pip3 install asyncio
 pip3 install discord_webhook
+
 ECHO Packages installed! Opening repo...
 START https://github.com/AstroOrbis/DiscordFuncs
+
 PAUSE
